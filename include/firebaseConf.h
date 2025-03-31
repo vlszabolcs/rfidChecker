4d417a4bd3c8f2b0525aed01046e9414f11270da
vlszabolcs <vereslakosszabolcs@gmail.com>
2025-03-31 16:07:42 +0200

    changed auth

--- include/firebaseConf.h
+++ include/firebaseConf.h
@@ -1,28 +1,43 @@
 #include <Firebase_ESP_Client.h>
-
-// Provide the token generation process info.
 #include "addons/TokenHelper.h"
-// Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
-// Define Firebase Data object
+// Firebase objektumok
 FirebaseData fbdo;
-
 FirebaseAuth auth;
 FirebaseConfig config;
 
-unsigned long sendDataPrevMillis = 0;
-int count = 0;
-bool signupOK = false;
-
+// Token frissítése
 void reconnectToFirebase()
 {
-  if (Firebase.isTokenExpired()){
+  if (Firebase.isTokenExpired())
+  {
     Firebase.refreshToken(&config);
-    Serial.println("Refresh token");
+    Serial.println("Token refreshed");
   }
 }
 
+// Firebase hibakezelés
+void handleFirebaseError(String errorReason)
+{
+  if (errorReason == "path not exist")
+  {
+    Serial.println("This card is unknown");
+    u8x8.clearDisplay();
+    u8x8.setCursor(0, 3);
+    u8x8.println("Unknown card");
+  }
+  else if (errorReason.indexOf("token is not ready") != -1)
+  {
+    Serial.printf("Hiba az adatok lekérdezésekor: %s\n", errorReason.c_str());
+    Serial.println("Token expired or revoked, reconnecting...");
+    u8x8.clearDisplay();
+    u8x8.setCursor(0, 3);
+    u8x8.println("RTDB error");
+  }
+}
+
+// Időbélyeg formázása
 String formatTimestamp(int timestamp)
 {
   time_t rawtime = timestamp;
@@ -32,87 +47,57 @@ String formatTimestamp(int timestamp)
   return String(buffer);
 }
 
-
-
-// Funkció az adatok lekérdezésére
+// Felhasználói adatok lekérdezése
 bool getUserData(String userId)
 {
   String path = userPath;
   path += userId;
   userData.uid = userId;
-  Serial.print("Adatok lekérése innen: ");
-  Serial.println(path);
+
+  Serial.printf("Adatok lekérése innen: %s\n", path.c_str());
   reconnectToFirebase();
-  // Adatok lekérése
+
   if (Firebase.RTDB.getJSON(&fbdo, path.c_str()))
   {
     if (fbdo.dataType() == "json")
     {
-      Serial.println("Adatok sikeresen lekérve!");
-
       FirebaseJson json = fbdo.jsonObject();
       FirebaseJsonData jsonData;
 
-      // Kiolvasás és kiírás
       if (json.get(jsonData, "credit"))
-      {
         userData.credit = jsonData.intValue;
-      }
-
       if (json.get(jsonData, "name"))
-      {
         userData.name = jsonData.stringValue;
-      }
-
       if (json.get(jsonData, "update"))
-      {
         userData.time = jsonData.intValue;
-      }
       if (json.get(jsonData, "loan"))
-      {
         userData.loan = jsonData.intValue;
-      }
+
+      Serial.println("Adatok sikeresen lekérve!");
       return true;
     }
   }
   else
   {
-    if (fbdo.errorReason() == "path not exist")
-    {
-      Serial.print("This card is unknown");
-      u8x8.clearDisplay();
-      u8x8.setCursor(0, 3);
-      u8x8.println("Unknown card");
-    }
-    else if (fbdo.errorReason().indexOf("token is not ready") != -1)
-    {
-      Serial.print("Hiba az adatok lekérdezésekor: ");
-      Serial.println(fbdo.errorReason());
-      Serial.println("Token expired or revoked, reconnecting...");
-      u8x8.clearDisplay();
-      u8x8.setCursor(0, 3);
-      u8x8.println("RTDB error");
-    }
+    handleFirebaseError(fbdo.errorReason());
     return false;
   }
+  return false;
 }
 
+// Felhasználói adatok frissítése
 void updateUserData(String userId)
 {
   String path = userPath;
   path += userId;
-
-  // JSON objektum létrehozása
   FirebaseJson json;
+
   json.set("credit", userData.credit);
   json.set("name", userData.name);
   json.set("update", userData.time);
   json.set("loan", userData.loan);
 
-  // Adatok küldése Firebase-be
-  Serial.print("Adatok frissítése a Firebase-ben: ");
-  Serial.println(path);
-  
+  Serial.printf("Adatok frissítése a Firebase-ben: %s\n", path.c_str());
   reconnectToFirebase();
 
   if (Firebase.RTDB.setJSON(&fbdo, path.c_str(), &json))
@@ -121,74 +106,55 @@ void updateUserData(String userId)
   }
   else
   {
-    Serial.print("Hiba az adatok frissítésekor: ");
-    Serial.println(fbdo.errorReason());
+    handleFirebaseError(fbdo.errorReason());
   }
 }
 
+// Felhasználói műveletek naplózása
 void logUserAction(String userId, int action, int remainingCredit)
 {
-  // Get the current timestamp
-  time_t now = timeClient.getEpochTime();
-  struct tm *timeinfo = localtime(&now);
-  String timestamp = String(now);
-  // Create the log path with the current year
+  String timestamp = String(timeClient.getEpochTime());
   String path = logPath;
-
   path += timestamp;
-
-  // Create the JSON object for the log
   FirebaseJson json;
   json.set("userId", userId);
   json.set("action", action);
   json.set("remainingCredit", remainingCredit);
-  
-  // Send the log to Firebase
-  Serial.print("Log bejegyzés készítése a Firebase-ben: ");
-  Serial.println(path);
+
+  Serial.printf("Log bejegyzés készítése a Firebase-ben: %s\n", path.c_str());
   reconnectToFirebase();
+
   if (Firebase.RTDB.setJSON(&fbdo, path.c_str(), &json))
   {
     Serial.println("Log bejegyzés sikeresen létrehozva!");
   }
   else
   {
-    Serial.print("Hiba a log bejegyzés létrehozásakor: ");
-    Serial.println(fbdo.errorReason());
+    handleFirebaseError(fbdo.errorReason());
   }
 }
 
+// Firebase konfiguráció inicializálása
 void firebaseConfig()
 {
-  /* Assign the api key (required) */
   config.api_key = API_KEY;
-
-  /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 
-  /* Sign up */
-  if (Firebase.signUp(&config, &auth, "", ""))
+  if (Firebase.signUp(&config, &auth, USER_EMAIL, USER_EMAIL))
   {
-    Serial.println("ok");
-    signupOK = true;
+    Serial.println("Sikeres bejelentkezés!");
+    Serial.print("ID Token: ");
+    Serial.println(auth.token.uid.c_str());
   }
   else
   {
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    Serial.printf("Signup error: %s\n", config.signer.signupError.message.c_str());
   }
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-   // Assign the maximum retry of token generation
-   config.max_token_generation_retry = 5;
-
-  Firebase.begin(&config, &auth);
-
-  if (!Firebase.RTDB.beginStream(&fbdo, "/conf"))
-  {
-    Serial.print("Hiba a stream elindításában: ");
-    Serial.println(fbdo.errorReason());
-  }
-
-  Firebase.reconnectWiFi(true);
+  config.token_status_callback = tokenStatusCallback;
+  config.max_token_generation_retry = 5;
 }
