d995ce5252105ae99e4be488588a38ae10c2adfe
vlszabolcs <vereslakosszabolcs@gmail.com>
2025-03-31 16:06:46 +0200

    formatted

--- include/wifiConf.h
+++ include/wifiConf.h
@@ -3,7 +3,7 @@
  *  WiFi > WiFiMulti: https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/examples/WiFiMulti/WiFiMulti.ino
  *  WiFi > WiFiScan: https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/examples/WiFiScan/WiFiScan.ino
  *  Complete project details at our blog: https://RandomNerdTutorials.com/
- *  
+ *
  */
 
 #include <WiFi.h>
@@ -16,9 +16,8 @@ WiFiMulti wifiMulti;
 extern NTPClient timeClient;
 
 
-const uint32_t connectTimeoutMs = 10000;
-
-void wifiConfig(){
+void wifiConfig()
+{
   WiFi.mode(WIFI_STA);
   // Add list of wifi networks
   wifiMulti.addAP(ssid1, pw1);
@@ -28,14 +27,16 @@ void wifiConfig(){
   // Connect to Wi-Fi using wifiMulti (connects to the SSID with strongest connection)
   Serial.println("Connecting Wifi...");
   u8x8.println("Connecting Wifi...");
-  
-  if(wifiMulti.run() == WL_CONNECTED) {
+  if (wifiMulti.run() == WL_CONNECTED)
+  {
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
     Serial.print("RSSI: ");
     Serial.println(WiFi.RSSI());
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
 
     u8x8.println("WiFi connected");
     u8x8.println("IP address: ");
@@ -43,11 +44,12 @@ void wifiConfig(){
     u8x8.println(WiFi.SSID());
     u8x8.print("RSSI: ");
     u8x8.println(WiFi.RSSI());
-    
+
     u8x8.refreshDisplay();
   }
 }
 
-void ntpConf(){
-    timeClient.begin();
+void ntpConf()
+{
+  timeClient.begin();
 }
