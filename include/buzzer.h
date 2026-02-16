d6c0ea75f56b3a5297d4ef42ff0578cce2d2d8b1
vlszabolcs <vereslakosszabolcs@gmail.com>
2026-02-16 15:57:29 +0100

    format

--- include/buzzer.h
+++ include/buzzer.h
@@ -7,17 +7,19 @@ void beepAccept()
 {
     tone(buzzer, 600); // Send 1KHz sound signal...
     delay(500);        // ...for 1 sec
-    noTone(buzzer);     // Stop sound...
+    noTone(buzzer);    // Stop sound...
 }
 
 void beepError()
 {
-    for(int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         tone(buzzer, 1000); // Send 1KHz sound signal...
-        delay(300);        // ...for 1 sec
+        delay(300);         // ...for 1 sec
         noTone(buzzer);     // Stop sound...
-        if(i < 2) {
-            delay(300);    // Add a small delay between beeps
+        if (i < 2)
+        {
+            delay(300); // Add a small delay between beeps
         }
     }
 }
--- include/rfidConf.h
+++ include/rfidConf.h
@@ -1,32 +1,37 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         4           // Configurable, see typical pin layout above
-#define SS_PIN          2          // Configurable, see typical pin layout above
+#define RST_PIN 4 // Configurable, see typical pin layout above
+#define SS_PIN 2  // Configurable, see typical pin layout above
 
-MFRC522 rfid(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 instance.
 
 MFRC522::MIFARE_Key key;
 
 void beepAccept();
 void beepError();
 
-String getUID() {
+String getUID()
+{
   // Check if a card is present
-  if (!rfid.PICC_IsNewCardPresent()) {
+  if (!rfid.PICC_IsNewCardPresent())
+  {
     return ""; // No card detected
   }
 
   // Read the card
-  if (!rfid.PICC_ReadCardSerial()) {
+  if (!rfid.PICC_ReadCardSerial())
+  {
     return ""; // Failed to read card
     beepError();
   }
 
   // Convert UID to string
   String uid = "";
-  for (byte i = 0; i < rfid.uid.size; i++) {
-    if (rfid.uid.uidByte[i] < 0x10) {
+  for (byte i = 0; i < rfid.uid.size; i++)
+  {
+    if (rfid.uid.uidByte[i] < 0x10)
+    {
       uid += "0"; // Add leading zero if needed
     }
     uid += String(rfid.uid.uidByte[i], HEX); // Convert byte to HEX
@@ -40,13 +45,16 @@ String getUID() {
   return uid;
 }
 
-void dump_byte_array(byte *buffer, byte bufferSize) {
-    for (byte i = 0; i < bufferSize; i++) {
-        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        Serial.print(buffer[i], HEX);
-    }
+void dump_byte_array(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
 }
 
-void rfidConfig(){
-    rfid.PCD_Init(); // Init MFRC522 card
+void rfidConfig()
+{
+  rfid.PCD_Init(); // Init MFRC522 card
 }
